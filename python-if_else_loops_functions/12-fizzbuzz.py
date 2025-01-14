473c580364f6865d0360a2a44b26e3c7e6355571
THO-Gaetan <9771@holbertonstudents.com>
2025-01-14 16:17:14 +0100

    trying another syntaxe

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,10 +1,10 @@
 def fizzbuzz():
     for i in range(1, 101):
         if i % 3 == 0 and i % 5 == 0:
-            print("{}".format("FizzBuzz"), end=' ')
+            print("FizzBuzz", end=' ')
         elif i % 3 == 0:
-            print("{}".format("Fizz"), end=' ')
+            print("Fizz", end=' ')
         elif i % 5 == 0:
-            print("{}".format("Buzz"), end=' ')
+            print("Buzz", end=' ')
         else:
-            print('{}'.format(i), end=' ')
+            print(i, end=' ')
