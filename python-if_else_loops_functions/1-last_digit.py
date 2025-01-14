b3139820c42bf8ebd54dfccf781970632c4fb09e
THO-Gaetan <9771@holbertonstudents.com>
2025-01-14 11:30:38 +0100

    fixing code style for exercise 1 and syntaxe for 0

--- python-if_else_loops_functions/1-last_digit.py
+++ python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 if number < 0:
-    last_digit = int(str(number)[-1]) * -1
+    last_dig = int(str(number)[-1]) * -1
 else:
-    last_digit = int(str(number)[-1])
-if last_digit > 5:
-    print(f"Last digit of {number} is {last_digit} and is greater than 5")
-elif last_digit == 0:
-    print(f"Last digit of {number} is {last_digit} and is 0")
-elif last_digit < 6:
-    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
+    last_dig = int(str(number)[-1])
+if last_dig > 5:
+    print(f"Last digit of {number} is {last_dig} and is greater than 5")
+elif last_dig == 0:
+    print(f"Last digit of {number} is {last_dig} and is 0")
+elif last_dig < 6:
+    print(f"Last digit of {number} is {last_dig} and is less than 6 and not 0")
