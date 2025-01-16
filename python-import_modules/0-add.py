86585efee56fa51975bba9387d183d832ff9d3a6
THO-Gaetan <9771@holbertonstudents.com>
2025-01-16 13:07:22 +0100

    fixing wrong syntaxe in the import and print

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 if __name__ == "__main__":
-	from 0-add import add
+	from add_0 import add
 a = 1
 b = 2
-print("{} + {} = {}".import(a, b, add(a, b)))
+print("{} + {} = {}".format(a, b, add(a, b)))
