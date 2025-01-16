a5d4b04e573fac1e59641321f125a1a18711a457
THO-Gaetan <9771@holbertonstudents.com>
2025-01-16 13:19:22 +0100

    first try for the exercise 1

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python3
-if __name__ == "__main__"
-	from calculator_1 import calculator
+if __name__ == "__main__":
+    from calculator_1 import add
 a = 10
 b = 5
-print
+print("{} + {} = {}".format(a, b, add(a, b)))
+if __name__ == "__main__":
+    from calculator_1 import sub
+print("{} - {} = {}".format(a, b, sub(a, b)))
+if __name__ == "__main__":
+    from calculator_1 import mul
+print("{} * {} = {}".format(a, b, mul(a, b)))
+if __name__ == "__main__":
+    from calculator_1 import div
+print("{} / {} = {}".format(a, b, div(a, b)))
