2603dfe5450c0a5fc372763f74814e8e09faaa71
THO-Gaetan <9771@holbertonstudents.com>
2025-01-17 14:27:20 +0100

    adding execution check when imported

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-import sys
-if len(sys.argv) - 1 == 0:
-    print("0 arguments.")
-elif len(sys.argv) - 1 == 1:
-    print("{} argument:".format(len(sys.argv) - 1))
-else:
-    print("{} arguments:".format(len(sys.argv) - 1))
-for index, args in enumerate(sys.argv[1:], start=1):
-    print("{}: {}".format(index, args))
+if __name__ == "__main__":
+    import sys
+    if len(sys.argv) - 1 == 0:
+        print("0 arguments.")
+    elif len(sys.argv) - 1 == 1:
+        print("{} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{} arguments:".format(len(sys.argv) - 1))
+    for index, args in enumerate(sys.argv[1:], start=1):
+        print("{}: {}".format(index, args))
