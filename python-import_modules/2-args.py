b75e717f353315d11a5ce5d637ba22d987d38959
THO-Gaetan <9771@holbertonstudents.com>
2025-01-17 14:24:45 +0100

    adding condition if only 1 argument

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -2,6 +2,8 @@
 import sys
 if len(sys.argv) - 1 == 0:
     print("0 arguments.")
+elif len(sys.argv) - 1 == 1:
+    print("{} argument:".format(len(sys.argv) - 1))
 else:
     print("{} arguments:".format(len(sys.argv) - 1))
 for index, args in enumerate(sys.argv[1:], start=1):
