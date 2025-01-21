c32789b099d0e9b849b1127054309fbee977db55
THO-Gaetan <9771@holbertonstudents.com>
2025-01-21 15:43:14 +0100

    first try for exercise 1 to 9

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if len(sentence) < 1:
+        return None
+    lenght = len(sentence)
+    first = sentence[0]
+    return(lenght, first)
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    max_value = 0
+    if len(my_list) == 0:
+        return None
+    for integer in my_list:
+        if integer > max_value:
+            max_value = integer
+    return max_value
