5bb50706219d3f56de4eb3c2fee8a566bc5e8373
THO-Gaetan <9771@holbertonstudents.com>
2025-01-22 10:32:45 +0100

    trying another syntaxe

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_list = my_list.copy()
-    for i in range (0, len(new_list)):
-        if new_list[i] == search:
-            new_list[i] = replace
-    return new_list
+    return (replace if number == search else number for number in my_list)
