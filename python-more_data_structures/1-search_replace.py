f328878ee904b91df1f267365a5106a76b1836b9
THO-Gaetan <9771@holbertonstudents.com>
2025-01-22 10:30:31 +0100

    adding an error condition

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-
 def search_replace(my_list, search, replace):
-    if search < 0 or search >= len(my_list):
-        return my_list
     new_list = my_list.copy()
     for i in range (0, len(new_list)):
         if new_list[i] == search:
