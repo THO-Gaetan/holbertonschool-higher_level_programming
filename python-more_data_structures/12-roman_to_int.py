39fbd5939367131a8a70cd59827ad0e586041465
THO-Gaetan <9771@holbertonstudents.com>
2025-01-23 09:46:09 +0100

    first try for exercise 7 to 12

--- python-more_data_structures/12-roman_to_int.py
+++ python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not roman_string:
+        return 0
+        result = 0
+    for char in roman_string:
+        if char == "I":
+            result += 1
+        elif char == "V":
+            result += 5
+        elif char == "X":
+            result += 10
+        elif char == "L":
+            result += 50
+        elif char == "C":
+            result += 100
+        elif char == "D":
+            result += 500
+        elif char == "M":
+            result += 1000
+    return result
+    
