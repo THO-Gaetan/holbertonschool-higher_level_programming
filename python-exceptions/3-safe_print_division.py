08fa6bb2e463b2b26a8a468710e63a6651dc3a9b
THO-Gaetan <9771@holbertonstudents.com>
2025-01-23 16:22:36 +0100

    fixing the division 0 management

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,12 +1,9 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        if b == 0:
-            result = None
-        else:
-            result = a / 2
-    except (ValueError, TypeError):
-        pass
+        result = a / 2
+    except ZeroDivisionError:
+        result = None
     finally:
         print("Inside result: {}".format(result))
         return result
