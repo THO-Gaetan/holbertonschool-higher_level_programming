def208f14ea5073c020d76ccd59476aeb77fdf98
THO-Gaetan <9771@holbertonstudents.com>
2025-01-23 16:50:40 +0100

    correct error message

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -6,7 +6,7 @@ def list_division(my_list_1, my_list_2, list_length):
             result = my_list_1[i] / my_list_2[i]
             new_list[i] = result
         except ZeroDivisionError:
-            print("Division by 0")
+            print("division by 0")
         except TypeError:
             print("wrong type")
         except IndexError:
