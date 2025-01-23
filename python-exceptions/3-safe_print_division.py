3f935345b7c51db8b8d4095d861196e9e88fbf1a
THO-Gaetan <9771@holbertonstudents.com>
2025-01-23 16:21:08 +0100

    first try exercise 3

--- python-exceptions/3-safe_print_division.py
+++ python-exceptions/3-safe_print_division.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    try:
+        if b == 0:
+            result = None
+        else:
+            result = a / 2
+    except (ValueError, TypeError):
+        pass
+    finally:
+        print("Inside result: {}".format(result))
+        return result
