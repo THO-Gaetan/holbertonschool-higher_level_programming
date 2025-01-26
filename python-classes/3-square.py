bd2cea4af20894d59a30b48b4668d520acb2a535
THO-Gaetan <9771@holbertonstudents.com>
2025-01-27 00:51:59 +0100

    adjusting for it to be more logical with what was asked

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,11 +10,3 @@ class Square:
     def area(self):
         """Calculate the area of the square."""
         return self.__size ** 2
-
-    def perimeter(self):
-        """Calculate the perimeter of the square."""
-        return 4 * self.__size
-
-    def get_size(self):
-        """Return the size of the square."""
-        return self.__size
