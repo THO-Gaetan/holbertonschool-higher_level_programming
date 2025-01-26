209bd7e4f97ac598db5cc4c70dcce5ac6969bfc5
THO-Gaetan <9771@holbertonstudents.com>
2025-01-27 00:51:09 +0100

    adjusting for it to be more logical with what was asked

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,16 +1,5 @@
 #!/usr/bin/python3
 class Square:
     def __init__(self, size):
-        self.__size = size  # Private instance attribute
-
-    def area(self):
-        """Calculate the area of the square."""
-        return self.__size ** 2
-
-    def perimeter(self):
-        """Calculate the perimeter of the square."""
-        return 4 * self.__size
-
-    def get_size(self):
-        """Return the size of the square."""
-        return self.__size
+        # Private instance attribute
+        self.__size = size
