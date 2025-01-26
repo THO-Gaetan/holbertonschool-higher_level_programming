4a5915dd999a7ba4ce3fa2010e1a976509ea2d18
THO-Gaetan <9771@holbertonstudents.com>
2025-01-27 00:51:33 +0100

    adjusting for it to be more logical with what was asked

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -6,15 +6,3 @@ class Square:
         if size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size  # Private instance attribute
-
-    def area(self):
-        """Calculate the area of the square."""
-        return self.__size ** 2
-
-    def perimeter(self):
-        """Calculate the perimeter of the square."""
-        return 4 * self.__size
-
-    def get_size(self):
-        """Return the size of the square."""
-        return self.__size
