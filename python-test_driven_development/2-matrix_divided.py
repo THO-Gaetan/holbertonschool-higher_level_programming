bcc5678c046861aac88c82d4e76611d9e53277ae
THO-Gaetan <9771@holbertonstudents.com>
2025-01-26 11:30:29 +0100

    adding the TypeError that only int or float are valid

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -10,6 +10,8 @@ def matrix_divided(matrix, div):
     if not isinstance(matrix, list):
         if not all(isinstance(row, list) for row in matrix):
             raise TypeError(err_msg)
+    if not all(isinstance(el, (int, float)) for row in matrix for el in row):
+        raise TypeError(err_msg)
     # if all the row in the matrix doesn't have the same size raise a TypeError
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
