c8a04b1bb0265579e7536309bfbc658ebf687624
THO-Gaetan <9771@holbertonstudents.com>
2025-01-26 17:14:46 +0100

    first try for exercise 4

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Module containing text_indentation function"""
+
+
+def text_indentation(text):
+    """Function to print a text with a new line when specific encouter"""
+    new_line = 0
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    for string in text:
+        if string == "." or string == "?" or string == ":":
+            new_line = 1
+            print("{}".format(string))
+        else:
+            if new_line == 1:
+                print()
+                new_line = 0
+            else:
+                print("{}".format(string), end="")
