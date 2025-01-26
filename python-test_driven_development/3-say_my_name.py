d1b31fd9d91bc3548d5fe694c8241bfce7eb4f08
THO-Gaetan <9771@holbertonstudents.com>
2025-01-26 16:49:42 +0100

    first try for exercise 2 along with the txt

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Module containing say_my_name function"""
+
+
+def say_my_name(first_name, last_name=""):
+    """Function that print first_name and last_name, only if both are string"""
+    err_msg = "first_name must be a string or last_name must be a string"
+    # Check first if first_name or last_name are a string or not
+    if not isinstance(first_name, str) or not isinstance(last_name, str):
+        raise TypeError(err_msg)
+    # print the first and last name
+    print("{} {}".format(first_name, last_name))
