02b690faec27f20d629a5fc0dcf2c745d7778f9e
THO-Gaetan <9771@holbertonstudents.com>
2025-01-26 16:58:40 +0100

    fixing wrong message error

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -4,9 +4,11 @@
 
 def say_my_name(first_name, last_name=""):
     """Function that print first_name and last_name, only if both are string"""
-    err_msg = "first_name must be a string or last_name must be a string"
+    err_msg = " or "
     # Check first if first_name or last_name are a string or not
-    if not isinstance(first_name, str) or not isinstance(last_name, str):
-        raise TypeError(err_msg)
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
     # print the first and last name
     print("My name is {} {}".format(first_name, last_name))
