aa6230929979c29321f409d8ccb2138dac2fdded
THO-Gaetan <9771@holbertonstudents.com>
2025-01-27 00:42:59 +0100

    change 0-square to be more logical and first update for 1-square and 2-square

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size  # Private instance attribute
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+
+    def perimeter(self):
+        """Calculate the perimeter of the square."""
+        return 4 * self.__size
+
+    def get_size(self):
+        """Return the size of the square."""
+        return self.__size
