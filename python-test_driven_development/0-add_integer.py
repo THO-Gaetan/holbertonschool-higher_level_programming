3efa0c1035f58819987a4ebc8af55f257cfe2ec5
THO-Gaetan <9771@holbertonstudents.com>
2025-01-26 10:59:22 +0100

    test for txt exercice 0

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+"""Module containing add_integer function"""
 def add_integer(a, b=98):
+    """Function to return a + b"""
+    # Checking if a and b are float to turn into int
     try:
         a = int(a) if isinstance(a, float) else a
         b = int(b) if isinstance(b, float) else b
         return a + b
     except TypeError:
+    # if float are neither an int or a float raise a TypeError
         if not isinstance(a, int):
             raise TypeError("a must be an integer")
         if not isinstance(b, int):
