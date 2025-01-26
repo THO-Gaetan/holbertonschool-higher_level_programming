55fe8e12d4a0430813b8389fcbb48a59872293fa
THO-Gaetan <9771@holbertonstudents.com>
2025-01-26 11:02:17 +0100

    pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Module containing add_integer function"""
+
+
 def add_integer(a, b=98):
     """Function to return a + b"""
     # Checking if a and b are float to turn into int
@@ -8,7 +10,7 @@ def add_integer(a, b=98):
         b = int(b) if isinstance(b, float) else b
         return a + b
     except TypeError:
-    # if float are neither an int or a float raise a TypeError
+        # if float are neither an int or a float raise a TypeError
         if not isinstance(a, int):
             raise TypeError("a must be an integer")
         if not isinstance(b, int):
