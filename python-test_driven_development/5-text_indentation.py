8577b3a10f7f00fc85a64edee0711d00b66046ea
THO-Gaetan <9771@holbertonstudents.com>
2025-01-26 17:38:28 +0100

    trying some modification to work with the txt test

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -4,20 +4,13 @@
 
 def text_indentation(text):
     """Function to print a text with a new line when specific encouter"""
-    new_line = 0
-    # check if text is a string or not
     if not isinstance(text, str):
         raise TypeError("text must be a string")
-    for string in text:
-        # if string is a ".", "?" or ":" we print it and go to the next line
-        if string == "." or string == "?" or string == ":":
-            new_line = 1
-            print("{}".format(string))
-        else:
-            # if we just got to the next line, we jump another line and
-            # don't count the next string
-            if new_line == 1:
-                print()
-                new_line = 0
-            else:
-                print("{}".format(string), end="")
+    punctuation = {'.', '?', ':'}
+    new_line = 0
+    for i, char in enumerate(text):
+        if char in punctuation:
+            print(text[new_line: i + 1].strip() + "\n")
+            new_line = i + 1
+    if new_line < len(text):
+        print(text[new_line:].strip(), end="")
