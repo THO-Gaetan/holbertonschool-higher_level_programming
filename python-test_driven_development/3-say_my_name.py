351ea68c412cc46fdcd3bdf81ceeb43db591d15b
THO-Gaetan <9771@holbertonstudents.com>
2025-01-26 16:56:33 +0100

    fixing the wrong output

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -9,4 +9,4 @@ def say_my_name(first_name, last_name=""):
     if not isinstance(first_name, str) or not isinstance(last_name, str):
         raise TypeError(err_msg)
     # print the first and last name
-    print("{} {}".format(first_name, last_name))
+    print("My name is {} {}".format(first_name, last_name))
