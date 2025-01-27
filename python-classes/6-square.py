d9fe9024d4e8152650a6db31641682e494234a5e
THO-Gaetan <9771@holbertonstudents.com>
2025-01-27 15:23:21 +0100

    first try for exercise 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,1 +1,48 @@
 #!/usr/bin/python3
+"""Module containing Square classe"""
+
+
+class Square:
+    """a class for a Square"""
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        """Get or set the size of the square."""
+        return self.__size
+    
+    @property
+    def position(self):
+        return self.__position
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @position.setter
+    def position(self, value):
+        if (not isinstance(value, tuple) or len(value) != 2 or
+                not all(isinstance(num, int) and num >= 0 for num in value)):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+            return
+        for i in range(self.position[1]):
+            print()
+        for i in range(self.size):
+            print(" " * self.position[0] + "#" * self.size)
+        
