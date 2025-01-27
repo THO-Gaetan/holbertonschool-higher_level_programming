b22741309712496a9125ff7e994f1a34627a1738
THO-Gaetan <9771@holbertonstudents.com>
2025-01-27 11:39:36 +0100

    first test for exercise 5

--- python-classes/5-square.py
+++ python-classes/5-square.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""Module containing Square classe"""
+
+
+class Square:
+    """a class for a Square"""
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        """Get or set the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Calculate the area of the square."""
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                for n in range(self.size):
+                    print("#", end="")
+                print()
+        
