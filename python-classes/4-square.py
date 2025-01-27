d4b7b9d5f2e907c584850228529d304eee11ea72
THO-Gaetan <9771@holbertonstudents.com>
2025-01-27 11:31:40 +0100

    first try for exercise 4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -5,20 +5,21 @@
 class Square:
     """a class for a Square"""
     def __init__(self, size=0):
-        if not isinstance(size, int):
+        self.size = size
+
+    @property
+    def size(self):
+        """Get or set the size of the square."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        self.__size = size  # Private instance attribute
+        self.__size = value
 
     def area(self):
         """Calculate the area of the square."""
         return self.__size ** 2
-
-    def perimeter(self):
-        """Calculate the perimeter of the square."""
-        return 4 * self.__size
-
-    def get_size(self):
-        """Return the size of the square."""
-        return self.__size
