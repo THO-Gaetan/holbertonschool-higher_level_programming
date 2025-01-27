8e52ed1c6aed3ceea558cf62645bb1f62b1d5c7e
THO-Gaetan <9771@holbertonstudents.com>
2025-01-27 11:15:11 +0100

    adding the first documentation

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module containing Square classe"""
+
+
 class Square:
+    """a class for a Square"""
     def __init__(self, size):
         # Private instance attribute
         self.__size = size
--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module containing Square classe"""
+
+
 class Square:
+    """a class for a Square"""
     def __init__(self, size=0):
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
