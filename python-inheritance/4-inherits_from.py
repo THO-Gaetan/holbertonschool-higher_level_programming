1bc6614a702651230cf1499165f8ec2ce71b4230
THO-Gaetan <9771@holbertonstudents.com>
2025-02-03 15:25:44 +0100

    fixing pycodestyle syntaxe

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,4 +4,4 @@
 
 def inherits_from(obj, a_class):
     """True if the object is an instance of a sub class"""
-    return issubclass(type(obj), a_class) and type(obj) != a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
