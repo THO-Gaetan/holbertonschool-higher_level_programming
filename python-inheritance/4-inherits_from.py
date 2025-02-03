32c5f419bc3669c843603dc585cd21e609e5914e
THO-Gaetan <9771@holbertonstudents.com>
2025-02-03 15:17:38 +0100

    first try for exercise 4

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,1 +1,5 @@
 #!/usr/bin/python3
+
+def inherits_from(obj, a_class):
+    """True if the object is an instance of a sub class"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
