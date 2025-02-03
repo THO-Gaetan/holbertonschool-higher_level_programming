d99019078c754e5809bf1c1074852c41c2111648
THO-Gaetan <9771@holbertonstudents.com>
2025-02-03 16:36:01 +0100

    first try exercise 10

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""create a class named Square"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """The square class that define the area of a square"""
+    def __init__(self, size):
+        """initialise the size of the square class"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
