62a2f10873e00670c7f3161e90b9e24d8458b716
THO-Gaetan <9771@holbertonstudents.com>
2025-02-03 15:34:24 +0100

    first try for exercise 7

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""creating a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """setting up the BaseGeometry with area def"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+        return value
