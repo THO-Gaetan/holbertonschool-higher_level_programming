00b876efdbc8be4b8602d6bb28204fcd87d92f2d
THO-Gaetan <9771@holbertonstudents.com>
2025-02-03 12:27:27 +0100

    first try

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""create the class MyList"""
+
+class MyList(list):
+    """initialise the list attribute"""
+    def __init__(self, iterable=None):
+        super().__init__(iterable or [])
+        
+    def print_sorted(self):
+        print(sorted(self))
