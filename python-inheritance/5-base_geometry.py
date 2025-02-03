24c1571821dbc45e6e076682acc8ebf224e92cc9
THO-Gaetan <9771@holbertonstudents.com>
2025-02-03 15:29:51 +0100

    first try exercise 5 and 6

--- python-inheritance/5-base_geometry.py
+++ python-inheritance/5-base_geometry.py
@@ -1,1 +1,7 @@
 #!/usr/bin/python3
+"""creating a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """setting up the BaseGeometry as empty"""
+    pass
--- python-inheritance/6-base_geometry.py
+++ python-inheritance/6-base_geometry.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""creating a BaseGeometry class"""
+
+
+class BaseGeometry:
+    """setting up the BaseGeometry with area def"""
+    def __init__(self):
+        pass
+
+    def area(self):
+        raise Exception("area() is not implemented")
