2a658101489dabe61e2ebb2fd3d9493853767388
THO-Gaetan <9771@holbertonstudents.com>
2025-02-03 16:37:56 +0100

    first try for exercise 11

--- python-inheritance/11-square.py
+++ python-inheritance/11-square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""create a class named Square"""
+
+
+Rectangle = __import__("9-rectangle").Rectangle
+
+
+class Square(Rectangle):
+    """The square class that define the area of a square"""
+    def __init__(self, size):
+        """initialise the size of the square class"""
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[square] {}/{}".format(self.__size, self.__size)
