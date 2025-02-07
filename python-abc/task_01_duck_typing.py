42d35ee444e148d20e3a3611074b89eeab443c79
THO-Gaetan <9771@holbertonstudents.com>
2025-02-07 17:57:38 +0100

    shpae info fixing syntaxe

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -36,5 +36,5 @@ class Rectangle(Shape):
 
 
 def shape_info(shape):
-    print("Area: {}".format(shape.area()))
-    print("Perimeter: {}".format(shape.perimeter()))
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
