658f2adf0e69e2b59c5fa7aa3282d0ade267bf4a
THO-Gaetan <9771@holbertonstudents.com>
2025-02-07 17:53:18 +0100

    putting private value instead of normal one

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -14,25 +14,25 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
-        self.radius = radius
+        self.__radius = radius
 
     def area(self):
-        return math.pi * self.radius ** 2
+        return math.pi * self.__radius ** 2
 
     def perimeter(self):
-        return 2 * math.pi * self.radius
+        return 2 * math.pi * self.__radius
 
 
 class Rectangle(Shape):
     def __init__(self, width, height):
-        self.width = width
-        self.height = height
+        self.__width = width
+        self.__height = height
 
     def area(self):
-        return self.width * self.height
+        return self.__width * self.__height
 
     def perimeter(self):
-        return 2 * (self.width + self.height)
+        return 2 * (self.__width + self.__height)
 
 
 def shape_info(shape):
