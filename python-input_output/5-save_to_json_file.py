f3ec476dbb63b78ed7079347e04a94eaa6bc1071
THO-Gaetan <9771@holbertonstudents.com>
2025-02-10 14:52:41 +0100

    first try for exercise 5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    with open("{}".format(filename), "w") as file:
+        result = json.dump(my_obj, file)
+        return result
