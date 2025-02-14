bb8ca0c8bec86bc95c3ba81b0fc2c16666a8edc6
THO-Gaetan <9771@holbertonstudents.com>
2025-02-14 11:42:33 +0100

    exercise 7 to 12

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+"""Module defining the Student class."""
+
+
+class Student:
+    """A class that defines a student."""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize a new Student instance."""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance."""
+        if attrs is None:
+            return self.__dict__
+        return {attr: getattr(self, attr) for attr in attrs
+                if hasattr(self, attr)}
--- python-input_output/8-class_to_json.py
+++ python-input_output/8-class_to_json.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Module for converting a class instance to a JSON-serializable dictionary."""
+
+
+def class_to_json(obj):
+    """
+    Returns the dictionary description of an object for JSON serialization.
+    """
+    result = {}
+    for key, value in obj.__dict__.items():
+        if isinstance(value, (list, dict, str, int, bool)):
+            result[key] = value
+    return result
