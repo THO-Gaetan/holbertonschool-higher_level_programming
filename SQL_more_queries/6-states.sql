eaa49f4fe7e3eb6dcb190b394d1c847d1618fba1
THO-Gaetan <9771@holbertonschool.com>
2025-03-04 14:02:08 +0000

    exercise 5 and 6

---
+++ SQL_more_queries/6-states.sql
@@ -0,0 +1,5 @@
+-- create the database hbtn_0d_usa
+-- create the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+Create TABLE IF NOT EXISTS states(id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256) NOT NULL);
