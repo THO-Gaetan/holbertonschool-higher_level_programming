49d7de86350ac82ea076b4777b5ef8cf9a12ac2d
THO-Gaetan <9771@holbertonschool.com>
2025-03-04 13:02:33 +0000

    fixe syntaxe

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,2 @@
 -- creates a mysql server user
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' INDENTIFIED BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
