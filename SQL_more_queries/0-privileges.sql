32b804aa9f7b18e3b145aab3719dc99aaabfc506
THO-Gaetan <9771@holbertonschool.com>
2025-03-04 12:57:11 +0000

    fixe exercise 0

--- SQL_more_queries/0-privileges.sql
+++ SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
--- lists all privileges of the mysql user
+-- list all privileges of the mysql user
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- list all privileges of the mysql user 2
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
