6d577e449599ae08c42b86911906368a3c3e5fcb
THO-Gaetan <9771@holbertonschool.com>
2025-03-04 14:39:50 +0000

    Fixe the syntaxe

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,9 @@
 -- lists all the cities of california tha can be found in hbtn_0d_usa
-SELECT cities.id, cities.name FROM cities where cities.state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY cities.id ASC;
+SELECT cities.id, cities.name
+FROM cities
+where cities.state_id = (
+      		      	SELECT id
+      		      	FROM states
+			WHERE name = 'California'
+)
+ORDER BY cities.id ASC;
