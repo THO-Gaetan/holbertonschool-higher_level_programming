92a6605f42b5e4a489e7c8c9cd475c259fe2ac53
THO-Gaetan <9771@holbertonschool.com>
2025-03-04 13:11:24 +0000

    fixing exercise 1

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,2 +1,5 @@
 -- creates a mysql server user
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
+-- Grant all privileges to user
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
