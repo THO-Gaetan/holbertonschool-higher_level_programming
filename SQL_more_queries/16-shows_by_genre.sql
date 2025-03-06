9fa438652aa644e1b2dd31a946a735bdb4e6b5fc
THO-Gaetan <9771@holbertonstudents.com>
2025-03-06 10:16:20 +0000

    Adding commentary

--- SQL_more_queries/16-shows_by_genre.sql
+++ SQL_more_queries/16-shows_by_genre.sql
@@ -1,3 +1,4 @@
+-- lists all shows and all genre linked to that show from hbtn_0d_tvshows
 SELECT tv_shows.title, tv_genres.name
 FROM tv_shows
 LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
