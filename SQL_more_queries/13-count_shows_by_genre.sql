1eb6cd413976db14c159fad623f963cac279ea2e
THO-Gaetan <9771@holbertonstudents.com>
2025-03-06 09:35:50 +0000

    fixing wrong dotes by underscore

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,6 +3,6 @@
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id)
 AS number_of_shows
 From tv_genres
-INNER JOIN tv_show.genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
 ORDER BY number_of_shows DESC;
