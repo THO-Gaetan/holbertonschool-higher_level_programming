a749c6c9cdf43ba186f0c2a527dadbd9f9e3fbfa
THO-Gaetan <9771@holbertonstudents.com>
2025-03-06 09:33:46 +0000

    fixing wrong from

--- SQL_more_queries/13-count_shows_by_genre.sql
+++ SQL_more_queries/13-count_shows_by_genre.sql
@@ -2,7 +2,7 @@
 -- display the number of shows linked
 SELECT tv_genres.name AS genre, COUNT(tv_show_genres.show_id)
 AS number_of_shows
-From tv_shows
+From tv_genres
 INNER JOIN tv_show.genres ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY tv_genres.id
 ORDER BY number_of_shows DESC;
