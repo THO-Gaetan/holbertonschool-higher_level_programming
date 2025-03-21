a42e06cc19d37517e645969c449bea7aad85dbd5
THO-Gaetan <9771@holbertonstudents.com>
2025-03-21 16:01:25 +0100

    adding th encoding system to the with open

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -20,5 +20,5 @@ def generate_invitations(template, attendees):
             processed_template = processed_template.replace(
                 placeholder, value if value else "N/A")
         output_file = f"output_{index}.txt"
-        with open(output_file, 'w') as file:
+        with open(output_file, 'w', encoding='utf-8') as file:
             file.write(processed_template)
