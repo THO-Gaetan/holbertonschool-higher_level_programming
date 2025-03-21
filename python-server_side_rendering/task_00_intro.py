1cd72e4b3dce8a4db324b8c35ffb278157963c57
THO-Gaetan <9771@holbertonstudents.com>
2025-03-21 15:48:10 +0100

    fixing wrong named variable processed.replace by processed_template.replace

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -15,7 +15,7 @@ def generate_invitations(template, attendees):
     for index, attendee in enumerate(attendees, 1):
         processed_template = template
         for key, value in attendee.items():
-            placeholder = ["name", "event_title", "event_date", "event_location"]
+            placeholder = f"{{{key}}}"
             processed_template = processed_template.replace(placeholder, value if value else "N/A")
         output_file = f"output_{index}.txt"
         with open(output_file, 'w') as file:
