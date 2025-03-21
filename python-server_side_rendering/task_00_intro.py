d7858a235fa89d30057b093cd9a590606109b709
THO-Gaetan <9771@holbertonstudents.com>
2025-03-21 15:50:03 +0100

    trying the pycodestyle

--- python-server_side_rendering/task_00_intro.py
+++ python-server_side_rendering/task_00_intro.py
@@ -1,23 +1,23 @@
 def generate_invitations(template, attendees):
 
     if not isinstance(template, str):
-        return("Error: The template is not a string")
-    
+        return ("Error: The template is not a string")
+
     if not isinstance(attendees, list):
-        return("Error: Attendees is not a list")
-    
+        return ("Error: Attendees is not a list")
+
     if not template.strip():
-        return("Error: The template can't be empty")
-    
+        return ("Error: The template can't be empty")
+
     if not attendees:
-        return("Error: The attendees list can't be empty")
+        return ("Error: The attendees list can't be empty")
 
     for index, attendee in enumerate(attendees, 1):
         processed_template = template
         for key, value in attendee.items():
             placeholder = f"{{{key}}}"
-            processed_template = processed_template.replace(placeholder, value if value else "N/A")
+            processed_template = processed_template.replace(
+                placeholder, value if value else "N/A")
         output_file = f"output_{index}.txt"
         with open(output_file, 'w') as file:
             file.write(processed_template)
-
